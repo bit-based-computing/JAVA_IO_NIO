7897b43aa1644cdfb824b84ce5e6f0b1e6e170f1
foysalahmmed <foysalahmmed73@gmail.com>
2020-05-04 21:31:56 +0600

    File operation example

---
+++ src/IO/FileExample.java
@@ -0,0 +1,23 @@
+package IO;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileExample {
+    public static void main(String[] args) {
+        File file = new File("input.txt");
+        if(file.exists()){
+            System.out.println("FIle is exist");
+        }else{
+            try{
+                file.createNewFile();
+            }catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        String workingDir = System.getProperty("user.dir");
+        System.out.println(workingDir);
+        System.out.println("It is file " + file.isFile());
+        System.out.println("It is directory " + file.isDirectory());
+    }
+}
